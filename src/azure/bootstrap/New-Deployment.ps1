7ce18c920b8c7750738caa00c68e08b37528c167
Pedro Paulo Vezza Campos <pedro@vezza.com.br>
2020-08-19 10:13:16 -0700

    Remove unused powershell script

--- src/azure/bootstrap/New-Deployment.ps1
+++
@@ -1,8 +0,0 @@
-Write-Information "Login to Azure"
-
-az login
-
-az account list --output table
-
-$subscription = Read-Host "Select the desired subscription"
-az account set --subscription $subscription
