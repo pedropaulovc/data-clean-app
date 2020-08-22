1d9da1421ea62c3cf7f2af5d89f7c5cbae8fe3d2
Pedro Paulo <pevezzac@microsoft.com>
2020-08-22 15:42:50 -0700

    Change deployment output to YAML (#5)

--- src/azure/continuous-deployment/Set-Deployment.ps1
+++ src/azure/continuous-deployment/Set-Deployment.ps1
@@ -1,15 +1,24 @@
+#!/usr/bin/env pwsh
+
 param (
-    [string] $deploymentResourceGroupPrefix = "rg-dataclean-deploy",
+    [string] $DeploymentResourceGroupPrefix = "rg-dataclean-deploy",
 
     [Parameter(Mandatory=$true)]
     [ValidateSet('dev','uat','ppe', 'prd')]
-    [string] $environment
+    [string] $Environment
 )
 
+$ErrorActionPreference = 'Stop'
+
+Set-PSRepository -Name "PSGallery" -InstallationPolicy Trusted
+Install-Module powershell-yaml
+
 $templateName = "DataCleanContinuousDeployment";
-$resourceGroupName = $deploymentResourceGroupPrefix + '-' + $environment;
-$deploymentName = $templateName + '-' + $environment + "-" + (Get-Date).ToUniversalTime().ToString('yyyyMMddTHHmm');
+$resourceGroupName = $DeploymentResourceGroupPrefix + '-' + $Environment;
+$deploymentName = $templateName + '-' + $Environment + "-" + (Get-Date).ToUniversalTime().ToString('yyyyMMddTHHmm');
 $templateFile = Join-Path $PSScriptRoot "$templateName.json";
 $templateParameters = Join-Path $PSScriptRoot "$templateName.parameters.json";
 
-az deployment group create --resource-group $resourceGroupName --template-file $templateFile --parameters $templateParameters --name $deploymentName
+az deployment group create --resource-group $resourceGroupName --template-file $templateFile --parameters $templateParameters --name $deploymentName `
+    | ConvertFrom-Json `
+    | ConvertTo-Yaml
