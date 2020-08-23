06ff18f3babbdb0e00772d26d01abe859d205b8a
Pedro Paulo Vezza Campos <pedro@vezza.com.br>
2020-08-22 20:33:29 -0700

    VNet + ACR + Log Insights + VNet diagnostics

---
+++ src/azure/continuous-deployment/westus2/Test-Deployment.ps1
@@ -0,0 +1,9 @@
+#!/usr/bin/env pwsh
+$ErrorActionPreference = 'Stop'
+
+$projectName = "dc" + (Get-Date).ToUniversalTime().ToString('HHmm');
+$resourceGroup = "rg-" + $projectName + "-westus2-dev";
+
+az group create --resource-group $resourceGroup --location westus2 | ConvertFrom-Json | ConvertTo-Yaml
+
+az deployment group create --resource-group $resourceGroup --template-file ./DataCleanPrimary.json --parameters ./DataCleanPrimary.parameters.json "projectName=$projectName" | ConvertFrom-Json | ConvertTo-Yaml
